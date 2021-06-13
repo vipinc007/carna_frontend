85dfa7c5683578a66309b1890729387a61086aac
vipin cheriyanveetil <vipinc.007@gmail.com>
2021-06-13 19:53:13 +0530

    added env file

--- src/Settings/AppSettings.jsx
+++ src/Settings/AppSettings.jsx
@@ -1,5 +1,4 @@
-const BACKEND_API_URL = "http://localhost:5000/";
-// const BACKEND_API_URL = "https://polaris-vipinc-backend-api.herokuapp.com/";
+const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
 
 export default {
   BACKEND_API_URL: BACKEND_API_URL,
